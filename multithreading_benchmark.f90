631c288e5008e9f097cdc9f990b86cb85f5cb39b
katgonzalez17 <katrina_gonzalez@college.harvard.edu>
2021-04-18 17:20:13 -0400

    Cleaned up code, added loops for easier benchmarking

--- multithreading_benchmark.f90
+++ multithreading_benchmark.f90
@@ -16,31 +16,39 @@ program multithreading_benchmark
 
 ! http://www.fftw.org/fftw3_doc/Fortran-Examples.html
 
-integer  :: N_test
+integer :: N_test
 integer :: iret
+integer :: N_threads, N_exp, curr_threads
 complex(C_DOUBLE_COMPLEX), allocatable :: tu_in(:), tu_out(:)
 real :: start, finish
 
-N_test = 33554432
 ! Create FFT plans
 iret = fftw_init_threads()
 ! Uncomment below to check if init_threads working (should be nonzero)
-! PRINT *, "iret: ", iret 
+PRINT *, "iret: ", iret 
 
-call fftw_plan_with_nthreads(1)
-plan = fftw_plan_dft_1d(N_test,tu_in,tu_out, FFTW_FORWARD,FFTW_ESTIMATE)
+do N_exp = 7,20
+    N_test = 2**N_exp
+    print '("Points: ",i12)', N_test
+    curr_threads = 2
+    do N_threads = 0,10
+        curr_threads = 2**N_threads
+        call fftw_plan_with_nthreads(curr_threads)
+        plan = fftw_plan_dft_1d(N_test,tu_in,tu_out, FFTW_FORWARD,FFTW_ESTIMATE)
 
-allocate(tu_in(N_test), stat=alloc_err)
-allocate(tu_out(N_test), stat=alloc_err)
-tu_in    = (0.0_dp, 0.0_dp)
-tu_out    = (0.0_dp, 0.0_dp)
+        allocate(tu_in(N_test), stat=alloc_err)
+        allocate(tu_out(N_test), stat=alloc_err)
+        tu_in    = (0.0_dp, 0.0_dp)
+        tu_out    = (0.0_dp, 0.0_dp)
 
-call cpu_time(start)
-call fftw_execute_dft(plan, tu_in, tu_out)
-call cpu_time(finish)
-print '("Time = ",f6.3," seconds.")',finish-start
-call fftw_destroy_plan(plan)
-
+        call cpu_time(start)
+        call fftw_execute_dft(plan, tu_in, tu_out)
+        call cpu_time(finish)
+        print '("Threads = ",i5,"   Time = ",f6.3," seconds.")',curr_threads,finish-start
+        call fftw_destroy_plan(plan)
+        deallocate(tu_in, tu_out)
+    end do
+end do
 ! Each output line contains results for an N
 ! results for each thread time for a single N comma separated
 
