1b114f3602aedbc9059018dd537e3d866acf81a7
Ubuntu <ubuntu@ip-172-31-55-104.ec2.internal>
2021-05-07 14:24:28 +0000

    tweak aws scrip

--- aws_setup.sh
+++ aws_setup.sh
@@ -6,8 +6,6 @@ sudo apt-get install gcc
 sudo apt install gfortran
 sudo apt-get install libblas-dev liblapack-dev
 sudo apt-get install libfftw3-dev
-sudo apt install python3-pip
-pip3 install matplotlib
 mkdir vtkdata
 sudo apt install libblacs-mpi-dev
 
