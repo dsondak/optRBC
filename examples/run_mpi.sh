202ec6c9fd5a4b33dae4ea037e94eae9a7c38562
Ubuntu <ubuntu@ip-172-31-55-104.ec2.internal>
2021-05-07 15:55:30 +0000

    mpi strong example

--- examples/run_mpi.sh
+++ examples/run_mpi.sh
@@ -1,3 +1,24 @@
+cd ..
+make
+cp time_loop.exe examples/
+cp time_loop_MPI.exe examples/
+cd examples/
+
+text="7.0, 1.5585, 0.1
+5.0e+03, 1, 1.1
+0.1, 0.01
+-1.0, 1.0
+1280, 960, 1
+0, 0, 0
+0, 0, 0"
+echo -e "$text" > input.data
+
+export OMPI_MCA_btl=^openib
+
+echo "########################"
+echo "#  MPI strong scaling  #"
+echo "########################"
+
 export OMP_NUM_THREADS=1
 echo "Serial version running ..."
 time ./time_loop.exe > out
@@ -12,3 +33,6 @@ time mpirun -np 5 ./time_loop_MPI.exe > out
 echo "6 processors running ..."
 time mpirun -np 6 ./time_loop_MPI.exe > out
 echo "done!"
+
+rm Nu_data.txt
+rm input.data
